0554ec09b420adbc861036788e94ef0687bb6899
Lkasta <lkastabackup@gmail.com>
2025-07-03 16:52:54 -0300

    feat(tasks): created delete and update task method

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__)
-
 tasks = []
-
 task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
@@ -33,7 +31,38 @@ def get_task(id):
         print(t)
         if t.id == id:
           return jsonify(t.to_dict())
-    return jsonify({"message": "No task found with that id"}), 404
+    return jsonify({"message": f"No task found with that id: {id}"}), 404
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+  task = None
+  for t in tasks:
+      if t.id == id:
+        task = t
+        break
+      
+  if not task:
+    return jsonify({"message": f"No task found with that id: {id}"}), 404
+    
+  data = request.get_json()
+  task.title = data['title']
+  task.description = data['description']
+  task.completed = data['completed']
+  return jsonify({"message": f"Updated task with id: {id}"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+  task = None
+  for t in tasks:
+      if t.id == id:
+        task = t
+        break
+
+  if not task:
+    return jsonify({"message": f"No task found with that id: {id}"}), 404
+  
+  tasks.remove(task)
+  return jsonify({"message": f"Deleted task with id: {id}"})
 
 if __name__ == "__main__":
   app.run(debug=True)
