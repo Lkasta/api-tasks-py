ee6bb331f8c2c0e084ef739e94c72772beb68feb
Lkasta <lkastabackup@gmail.com>
2025-07-03 16:03:10 -0300

    feat(tasks): added get method

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def initial():
-  return "Hello Jonas"
+tasks = []
+
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(id=task_id_control ,title="title", description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({"message": "New task created successfully"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list = [task.to_dict() for task in tasks]
+
+  output = {
+    "tasks": task_list,
+    "total_tasks": len(task_list)
+  }
+  return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+    print(t)
+    if t.id == id:
+      return jsonify(t.to_dict())
+    return jsonify({"message": f"No task found with that id: {id}"}), 404
 
 if __name__ == "__main__":
   app.run(debug=True)
